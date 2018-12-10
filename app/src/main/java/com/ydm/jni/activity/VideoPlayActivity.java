5f39c8c5b74b020dca992ea4fe0d0bf7f5d50b0d
DerMing <udeming1573@163.com>
2018-12-10 22:09:44 +0800

    基类+头部布局

---
+++ app/src/main/java/com/ydm/jni/activity/VideoPlayActivity.java
@@ -0,0 +1,10 @@
+package com.ydm.jni.activity;
+
+/**
+ * Description：视频播放
+ * Author：DerMing_You
+ * Email：udeming1573@163.com
+ * Date：2018/12/10  21:30
+ */
+public class VideoPlayActivity {
+}
---
+++ app/src/main/java/com/ydm/jni/base/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.ydm.jni.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description：
+ * Author：DerMing_You
+ * Email：udeming1573@163.com
+ * Date：2018/12/10  21:31
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
