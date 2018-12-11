2a528289bf5986714a72e9f0a88b71094b5c99f7
尤德铭 <youdm@1jbest.cn>
2018-12-11 11:21:29 +0800

    基类调整

--- app/src/main/java/com/ydm/jni/activity/VideoPlayActivity.java
+++ app/src/main/java/com/ydm/jni/activity/VideoPlayActivity.java
@@ -1,10 +1,30 @@
 package com.ydm.jni.activity;
 
+import android.os.Bundle;
+import android.view.View;
+
+import com.ydm.jni.base.BaseActivity;
+
 /**
  * Description：视频播放
  * Author：DerMing_You
  * Email：udeming1573@163.com
  * Date：2018/12/10  21:30
  */
-public class VideoPlayActivity {
+public class VideoPlayActivity extends BaseActivity {
+
+    @Override
+    public int bindLayout() {
+        return 0;
+    }
+
+    @Override
+    public void initView(Bundle savedInstanceState, View contentView) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
 }
--- app/src/main/java/com/ydm/jni/base/BaseActivity.java
+++ app/src/main/java/com/ydm/jni/base/BaseActivity.java
@@ -1,8 +1,16 @@
 package com.ydm.jni.base;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Description：
@@ -10,10 +18,36 @@ import android.support.v7.app.AppCompatActivity;
  * Email：udeming1573@163.com
  * Date：2018/12/10  21:31
  */
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Activity mContext;
+
+    protected View mContentView;
+    private Unbinder mUnbinder;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mContext = this;
+
+        setBaseView(bindLayout());
+
+        initView(savedInstanceState, mContentView);
+
+        initData();
+    }
+
+    public abstract int bindLayout();
+
+    public abstract void initView(final Bundle savedInstanceState, final View contentView);
+
+    public abstract void initData();
+
+    @SuppressLint("ResourceType")
+    protected void setBaseView(@LayoutRes int layoutId) {
+        if (layoutId <= 0) return;
+        setContentView(mContentView = LayoutInflater.from(this).inflate(layoutId, null));
+        mUnbinder = ButterKnife.bind(this);
     }
 }
